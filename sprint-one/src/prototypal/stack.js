8d6438188933d1ff041ca7608976c6b2bd76b444
cwillens <charwillens@gmail.com>
2016-09-14 16:10:44 -0700

    implemented stack prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var result = Object.create(stackMethods);
+  return result;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  storage: {},
+  counter: 0,
+  size: function() {
+    return this.counter;
+  },
+  push: function(item) {
+    this.storage[this.counter] = item;
+    this.counter ++;
+  },
+  pop: function() {
+    if (this.counter > 0) {
+      this.counter --;
+      return this.storage[this.counter];
+    }
+  }
+};
 
 
