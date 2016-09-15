cb21ba5a31b3aaa20d18fe71264b625ae5e0ae8b
cwillens <charwillens@gmail.com>
2016-09-14 20:40:03 -0700

    added what we think is a more classic prototypal version of this code in case we need to use it later (it's currently commented out)

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -23,3 +23,28 @@ var queueMethods = {
 };
 
 
+// var Queue = function() {
+//   var result = Object.create(Queue.prototype);
+//   result.counter = 0;
+//   result.storage = {};
+//   result.start = 0;
+//   return result;
+// };
+
+// Queue.prototype.size = function () {
+//   return this.counter - this.start;
+// };
+
+// Queue.prototype.enqueue = function(item) {
+//   this.storage[this.counter] = item;
+//   this.counter++;
+// };
+
+// Queue.prototype.dequeue = function() {
+//   if (this.size() > 0) {
+//     this.start++;
+//     return this.storage[this.start - 1];
+//   }
+// };
+
+
