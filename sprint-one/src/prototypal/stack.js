fc13c4485c159a9349ccd50bae875bb34bddfa3d
cwillens <charwillens@gmail.com>
2016-09-14 20:21:04 -0700

    added commented out version of prototypal stack. we need to figure out later why this makes the SpecRunner disappear

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -24,3 +24,27 @@ var stackMethods = {
 };
 
 
+// var Stack = function() {
+//  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+//   // but try not not reference your old code in writing the new style.
+//   var result = Object.create(Stack.prototype);
+//   result.storage = {};
+//   result.counter = 0;
+//   return result;
+// };
+
+// Stack.prototype.size = function() {
+//   return this.counter;
+// };
+
+// Stack.prototype.push = function(item) {
+//   this.storage[this.counter] = item;
+//   this.counter ++;
+// };
+
+// Stack.prototype.pop = function() {
+//   if (this.counter > 0) {
+//     this.counter --;
+//     return this.storage[this.counter];
+//   }
+// };
