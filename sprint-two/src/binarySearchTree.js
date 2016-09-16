e6ed7e282c97147a5332994e56b54876f2a83012
cwillens <charwillens@gmail.com>
2016-09-15 20:44:17 -0700

    Completed Binary Search

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,50 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(BinarySearchTree.prototype);
+  tree.value = value;
+  return tree;
 };
 
 
+BinarySearchTree.prototype.insert = function(value) {
+  if (this.value < value) {
+    if (this.right === undefined) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  } else if (this.value > value) {
+    if (this.left === undefined) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  if (this.value === value) {
+    return true;
+  } else if (value > this.value) {
+    if (this.right) {
+      return this.right.contains(value);
+    }
+  } else {
+    if (this.left) {
+      return this.left.contains(value);
+    }
+  }
+  return false;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  cb(this.value);
+  if (this.right) {
+    this.right.depthFirstLog(cb);
+  }
+  if (this.left) {
+    this.left.depthFirstLog(cb);
+  }
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
