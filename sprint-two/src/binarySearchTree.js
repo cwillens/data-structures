5bedd036a508a5cb2826ad178f8a12361923ce82
cwillens <charwillens@gmail.com>
2016-09-16 21:56:06 -0700

    Implemented calculating min and max depth for rebalance

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -21,6 +21,37 @@ BinarySearchTree.prototype.insert = function(value) {
   }
 };
 
+BinarySearchTree.prototype.depth = function (counter, depths) {
+  var depths = depths || {max: 1};
+  var counter = counter || 0;
+  counter ++;
+//  var counter = 1;
+  //debugger;
+  if (this.right === undefined && this.left === undefined) {
+    //If this is the final node
+    if (counter > depths.max) {
+      //Check to see if the current depth is greater than the max depth
+      depths.max = counter;
+    } 
+    if (depths.min) {
+      if (counter < depths.min) { depths.min = counter; }
+    } else {
+      depths.min = counter;
+    }
+  } else {
+    //In the case that more branches exist
+    if (this.right) {
+      //counter++;
+      this.right.depth(counter, depths);
+    }
+    if (this.left) {
+      //counter++;
+      this.left.depth(counter, depths);
+    }
+  }
+  return depths;
+};
+
 BinarySearchTree.prototype.contains = function(value) {
   if (this.value === value) {
     return true;
